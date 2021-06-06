c52a46ed43e748d8434f30ee6aebba748edd9f71
Faaahdk <fahd.kabab@gmail.com>
2021-06-06 10:13:56 +0200

    Définition du GenericService

--- src/main/java/fr/jdr/spring/services/GenericService.java
+++ src/main/java/fr/jdr/spring/services/GenericService.java
@@ -1,5 +1,16 @@
 package fr.jdr.spring.services;
 
-public interface GenericService {
+import java.util.List;
 
+public interface GenericService<T> {
+
+	public List<T> getAll();
+	
+	public T getById(String id);
+	
+	public T create(T entity);
+	
+	public T update(T entity);
+	
+	public void delete(T entity);
 }
