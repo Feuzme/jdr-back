e509244414e7f7a8b98e33a192d0c9f2f756f55a
Faaahdk <fahd.kabab@gmail.com>
2021-06-06 10:32:52 +0200

    Définition des Services

--- src/main/java/fr/jdr/spring/config/ServiceConfig.java
+++ src/main/java/fr/jdr/spring/config/ServiceConfig.java
@@ -1,5 +1,32 @@
 package fr.jdr.spring.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import fr.jdr.spring.models.Creneau;
+import fr.jdr.spring.models.Game;
+import fr.jdr.spring.models.Session;
+import fr.jdr.spring.services.GenericService;
+import fr.jdr.spring.services.servicesimpl.CreneauServiceImpl;
+import fr.jdr.spring.services.servicesimpl.GameServiceImpl;
+import fr.jdr.spring.services.servicesimpl.SessionServiceImpl;
+
+@Configuration
 public class ServiceConfig {
+	
+	@Bean
+	public GenericService<Creneau> creneauServiceFactory(){
+		return new CreneauServiceImpl();
+	}
+	
+	@Bean
+	public GenericService<Game> gameServiceFactory(){
+		return new GameServiceImpl();
+	}
+	
+	@Bean
+	public GenericService<Session> sessionServiceFactory(){
+		return new SessionServiceImpl();
+	}
 
 }
