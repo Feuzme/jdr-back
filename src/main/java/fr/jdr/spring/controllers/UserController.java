7484264fb809d1b88edae39b8d914e64c5c060b9
Amir <slampas@hotmail.fr>
2021-06-07 02:12:31 +0200

    add anotation

--- src/main/java/fr/jdr/spring/controllers/UserController.java
+++ src/main/java/fr/jdr/spring/controllers/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -20,6 +21,7 @@ import fr.jdr.spring.services.UserService;
 
 @RestController
 @RequestMapping("users")
+@CrossOrigin
 public class UserController {
 	
 	
