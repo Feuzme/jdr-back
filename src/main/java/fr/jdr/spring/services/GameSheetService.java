d494db19f4b52a58bc75587fe0d5cfe2f57b6581
Fyfeu Feuzme <feuzme@gmail.com>
2021-06-08 09:39:45 +0200

    feat(dto) reorganized packages for dto

--- src/main/java/fr/jdr/spring/services/GameSheetService.java
+++ src/main/java/fr/jdr/spring/services/GameSheetService.java
@@ -4,6 +4,14 @@ import java.util.List;
 
 import fr.jdr.spring.dto.sheet.GameSheetDto;
 
-public interface GameSheetService extends GenericService<GameSheetDto> {
+public interface GameSheetService {
+	public List<GameSheetDto> getAll();
 	
+	public GameSheetDto getById(String id);
+	
+	public GameSheetDto create(GameSheetDto entity);
+	
+	public GameSheetDto update(GameSheetDto entity);
+	
+	public void deleteById(String id);
 }
