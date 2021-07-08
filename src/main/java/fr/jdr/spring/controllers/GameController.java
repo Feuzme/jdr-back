6a9c9acb13c7106f3fed69f42de1066042aaebf4
Amir <slampas@hotmail.fr>
2021-07-08 14:41:24 +0200

    crossorigin added

--- src/main/java/fr/jdr/spring/controllers/GameController.java
+++ src/main/java/fr/jdr/spring/controllers/GameController.java
@@ -3,6 +3,7 @@ package fr.jdr.spring.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import fr.jdr.spring.models.Game;
 import fr.jdr.spring.services.GenericService;
-
+@CrossOrigin
 @RestController
 @RequestMapping("games")
 public class GameController {
--- src/main/java/fr/jdr/spring/controllers/SessionController.java
+++ src/main/java/fr/jdr/spring/controllers/SessionController.java
@@ -3,6 +3,7 @@ package fr.jdr.spring.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import fr.jdr.spring.models.Session;
 import fr.jdr.spring.services.GenericService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("sessions")
 public class SessionController {
